6a29054532024bf7769110bf7187992ee3b66e8e
Artur Trofimov <arturtr@gmail.com>
2016-03-10 14:32:43 +1000

    Clear open date before save issue if it open

--- lib/issue_open_date/patches/issue_patch.rb
+++ lib/issue_open_date/patches/issue_patch.rb
@@ -7,6 +7,17 @@ module IssueOpenDate
         safe_attributes 'open_date',
                         if: lambda { |issue, user| user.allowed_to?(:edit_issues, issue.project) }
 
+
+        before_save :clear_open_date
+
+        private
+
+        def clear_open_date
+          if self.open_date.present? and !self.closed?
+            self.open_date = nil
+          end
+        end
+
       end
     end
 
