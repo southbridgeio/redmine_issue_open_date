b9137fa1d0d441d42ccbdd90b5e1dbf0f309a113
Stepan Lusnikov <endenwer@gmail.com>
2016-03-21 19:17:09 +0300

    Fix calendar

--- init.rb
+++ init.rb
@@ -8,7 +8,7 @@ Redmine::Plugin.register :redmine_issue_open_date do
   name 'Redmine Issue Open Date plugin'
   url 'https://github.com/centosadmin/redmine_issue_open_date'
   description 'This is a plugin for Redmine which open freezed issues with specified oped date'
-  version '0.1.0'
+  version '0.1.1'
   author 'Centos-admin.ru'
   author_url 'http://centos-admin.ru'
   settings(default: {
