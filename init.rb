54fe166030ac4c508f21e9bd3b700d25406b5fb5
Igor Olemskoi <igor@southbridge.ru>
2019-11-19 22:41:47 +0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -11,7 +11,7 @@ Rails.application.config.eager_load_paths += [workers_path]
 
 Redmine::Plugin.register :redmine_issue_open_date do
   name 'Redmine Issue Open Date plugin'
-  url 'https://github.com/centosadmin/redmine_issue_open_date'
+  url 'https://github.com/southbridgeio/redmine_issue_open_date'
   description 'This is a plugin for Redmine which open freezed issues with specified oped date'
   version '0.3.0'
   author 'Southbridge'
