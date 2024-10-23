19a95d6f77498281ce0c7c08e05caf433f53086d
nevrfl <mvvolkov96@gmail.com>
2024-10-23 19:45:23 +0300

    add datetimepicker

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.3.2
+
+* Add datetimepicker
+
 # 0.3.1
 
 * Add compatibility with Redmine 5.1
--- lib/issue_open_date/patches/issue_patch.rb
+++ lib/issue_open_date/patches/issue_patch.rb
@@ -8,8 +8,16 @@ module IssueOpenDate
 
           before_save :clear_open_date
 
+          safe_attributes :open_date
+
           def open_date
             super.in_time_zone(User.current.time_zone) if super
+          end
+
+          def open_date=(value)
+            user = User.current
+            return super unless value && user.logged? && user.time_zone
+            super(value.to_datetime.change(offset: user.time_zone.formatted_offset))
           end
 
           private
