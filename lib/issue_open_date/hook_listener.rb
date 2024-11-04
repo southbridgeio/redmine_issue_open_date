691ac9b40812b443c5bba15dc786f2b2c2f578d9
nevrfl <mvvolkov96@gmail.com>
2024-11-04 13:45:44 +0300

    fix bulk issues update for datetimepicker

--- lib/issue_open_date/hook_listener.rb
+++ lib/issue_open_date/hook_listener.rb
@@ -4,26 +4,5 @@ module IssueOpenDate
     render_on :view_issues_bulk_edit_details_bottom, partial: 'issues/open_date_field_bulk'
     render_on :view_issues_show_details_bottom, partial: 'issues/open_date'
     render_on :view_my_account, partial: 'my/open_date'
-
-    def controller_issues_edit_before_save(context = {})
-      assign_issue_attributes(context)
-    end
-
-    def controller_issues_bulk_edit_before_save(context = {})
-      assign_issue_attributes(context)
-    end
-
-    def controller_issues_new_before_save(context = {})
-      assign_issue_attributes(context)
-    end
-
-    private
-
-    def assign_issue_attributes(context)
-      params, issue = context[:params].to_unsafe_h, context[:issue]
-      Time.use_zone(User.current.time_zone || Time.now.localtime.utc_offset / 3600) do
-        issue.assign_attributes(params[:issue].slice(*(1..5).map { |i| "open_date(#{i}i)" }))
-      end
-    end
   end
 end
